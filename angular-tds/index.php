d753ba15121384b0c79c6b161692cd5890005307
Maxime Beacco <Obscure-Angel@gmx.fr>
2017-01-27 11:01:36 +0100

    TP1
    Ajout  des librairies Angular et Bootstrap
    Ajout de l'index.php
    Ajout de l'app et du contrôleur

---
+++ angular-tds/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr" data-ng-app="NoteApp">
+
+	<head>
+		<title>Angular TDs</title>
+		<meta http-equiv="content-type" content="text/html" charset="utf-8">
+		<meta name="author" content="ObscureAngel">
+		<link type="text/css" rel="stylesheet" href="css/bootstrap.min.css" />
+		<script type="text/javascript" src="js/angular.min.js"></script>
+		<script type="text/javascript" src="app/noteApp.js"></script>
+		<script type="text/javascript" src="app/noteController.js"></script>
+	</head>
+	
+	<body>
+		
+	</body>
+
+</html>
