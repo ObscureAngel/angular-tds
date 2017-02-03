cfcdb36f84ebdde26d374ff6d7200fd140bdf1d5
Maxime Beacco <Obscure-Angel@gmx.fr>
2017-02-03 10:06:46 +0100

    Démarrage du module "Services"
    Ajout du module et du controller

---
+++ angular-tds/app/serviceController.js
@@ -0,0 +1,24 @@
+angular.module('ServiceApp', []).controller('ServiceController', function(){
+	
+	this.services = listeServices;
+	
+	var listeServices = [
+	    {
+	        "name": "Web Development",
+	        "price": 300,
+	        "active":true
+	    },{
+	        "name": "Design",
+	        "price": 400,
+	        "active":false
+	    },{
+	        "name": "Integration",
+	        "price": 250,
+	        "active":false
+	    },{
+	        "name": "Formation",
+	        "price": 220,
+	        "active":false
+	    }
+	]
+});
--- angular-tds/index.php
+++ angular-tds/index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="fr" data-ng-app="NoteApp">
+<html lang="fr">
 
 	<head>
 		<title>Angular TDs</title>
@@ -11,23 +11,33 @@
 		<script type="text/javascript" src="js/angular-cookies.min.js"></script>
 		<script type="text/javascript" src="app/noteApp.js"></script>
 		<script type="text/javascript" src="app/noteController.js"></script>
+		<script type="text/javascript" src="app/serviceApp.js"></script>
+		<script type="text/javascript" src="app/serviceController.js"></script>
 	</head>
 	
-	<body data-ng-controller="NoteController as noteCtrl">
-		<div class="container">
-			<form name="message" data-ng-submit="noteCtrl.addNote()">
-				<label for="messageNote">Note :</label>
+	<body>
+		<article data-ng-app="NoteApp">
+			<div class="container" data-ng-controller="NoteController as noteCtrl">
+				<form name="message" data-ng-submit="noteCtrl.addNote()">
+					<label for="messageNote">Note :</label>
+					<br>
+					<textarea id="messageNote" data-ng-trim="false" maxlength="100" data-ng-model="noteCtrl.messageNote" data-ng-change="noteCtrl.count()">{{noteCtrl.messageNote}}</textarea>
+				</form>
+				<span class="restant" data-ng-model="noteCtrl.messageLength">Nombre de carat&egrave;res restants : {{noteCtrl.messageLength}}</span>
 				<br>
-				<textarea id="messageNote" data-ng-trim="false" maxlength="100" data-ng-model="noteCtrl.messageNote" data-ng-change="noteCtrl.count()">{{noteCtrl.messageNote}}</textarea>
-			</form>
-			<span class="restant" data-ng-model="noteCtrl.messageLength">Nombre de carat&egrave;res restants : {{noteCtrl.messageLength}}</span>
-			<br>
-			<button data-ng-click="noteCtrl.save()">Enregistrer</button>
-			<div class="enregistre" data-ng-show="noteCtrl.status">
-				<span>Note enregistr&eacute;e</span>
+				<button data-ng-click="noteCtrl.save()">Enregistrer</button>
+				<div class="enregistre" data-ng-show="noteCtrl.status">
+					<span>Note enregistr&eacute;e</span>
+				</div>
+				
 			</div>
+		</article>
+		
+		<article data-ng-app="ServiceApp">
+			<div class="container" data-ng-controller="ServiceController as serviceCtrl">
 			
-		</div>
+			</div>
+		</article>
 	</body>
 
 </html>
