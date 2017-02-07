5a09ccb6c3a624bde0eca753b04e563f861e868c
Maxime Beacco <Obscure-Angel@gmx.fr>
2017-02-07 09:33:47 +0100

    Fin de la première partie du TD
    Comptage des services opérationnel
    Calcul du prix total opérationnel

--- angular-tds/app/serviceController.js
+++ angular-tds/app/serviceController.js
@@ -1,35 +1,66 @@
 angular.module('ServiceApp', []).controller('ServiceController', function(){
-	
-	this.products = listeServices;
-	this.prixTotal = 0;
-	this.nbrServices = 1;
-	
+
 	var listeServices = [
-	    {
-	        name : 'Web Development',
-	        price : 300,
-	        active : true
-	    },{
-	        name : 'Design',
-	        price : 400,
-	        active : false
-	    },{
-	        name : 'Integration',
-	        price : 250,
-	        active : false
-	    },{
-	        name : 'Formation',
-	        price : 220,
-	        active : false
-	    }
-	]
-	
+		{
+			number : 0,
+			name : 'Web Development',
+			price : 300,
+			active : true
+		},{
+			number : 1,
+			name : 'Design',
+			price : 400,
+			active : false
+		},{
+			number : 2,
+			name : 'Integration',
+			price : 250,
+			active : false
+		},{
+			number : 3,
+			name : 'Formation',
+			price : 220,
+			active : false
+		}
+		];
+
+	var promo = {
+			"B22":0.05,
+			"AZ":0.01,
+			"UBOAT":0.02
+	}
+
+	this.products = listeServices;
+	this.prixTotal = 300;
+	this.nbrServices = 1;
+	this.listePromo = promo;
+
 	this.total = function() {
+		var i;
 		this.prixTotal = 0;
+		for (i = 0; i < 4; i++) {
+			if (this.products[i].active == true) {
+				this.prixTotal = this.prixTotal + this.products[i].price;
+			};
+		};
+
 	};
-	
+
 	this.toggleActive = function(service) {
-		this.products[service].active = true;
+		if (this.products[service].active == true) {
+			this.nbrServices = this.nbrServices - 1;
+			this.products[service].active = false;
+			this.total();
+		} else {
+			this.nbrServices = this.nbrServices + 1;
+			this.products[service].active = true;
+			this.total();
+		}
+
 	};
 	
+	this.verifPromo = function() {
+		
+	};
+
 });
